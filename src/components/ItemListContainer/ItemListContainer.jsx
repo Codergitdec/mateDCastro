018f4939858301a1021a7b99a356fe546c3d95b9
Daniel Castro <dancastro83@yahoo.com.ar>
2022-07-14 16:11:58 -0300

    catalogo con maps

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,24 @@
-const ItemListContainer = ({Pregunta}) => {
-    return <p>{Pregunta}</p>;
+
+import { useState, useEffect } from "react";
+import ItemList from "../ItemList/ItemList";
+import data from "../database/data.json";
+
+const ItemListContainer = () => {
+  const [items, setItems] = useState([]);
+  const promise = new Promise((resolve) => {
+    setTimeout(() => resolve(data), 2000);
+  });
+
+  useEffect(() => {
+    promise.then((res) => setItems(data));
+  }, []);
+  return (
+    <>
+      <div className="mt-5">
+        <ItemList items={items} />
+      </div>
+    </>
+  );
 };
- 
+
 export default ItemListContainer;
